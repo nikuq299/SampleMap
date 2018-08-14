2379952688fadc2f23a1dadf91a6fa9984799e98
nikuq299 <webmaster@nikuq299.com>
2018-08-14 23:31:07 +0900

    first commit

---
+++ SampleMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleMap
+//
+//  Created by nikuq299 on 2018/08/12.
+//  Copyright © 2018年 nikuq299. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
